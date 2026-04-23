c9c86e6831d3f9396ce9dbdca3cda91745e0d9f3
Nigel Perks <nigelperks.github@gmail.com>
2026-04-23 03:29:23 +0100

    URM frontend: labels, GOTO

--- URMFE/urmfe.c
+++ URMFE/urmfe.c
@@ -1,4 +1,22 @@
-// URM front end
+// URM frontend
+// Nigel Perks, 2026
+
+// A frontend language for URM.
+// Avoid working with instruction numbers and register numbers.
+// Allocate instruction numbers to labels, register numbers to variables.
+
+// fred:
+//
+// zero foo                    --> Z(n)
+// succ foo                    --> S(n)
+// inc foo                     --> S(n)
+// copy foo to bar             --> C(n,m)
+// goto fred                   --> J(1,1,p)
+// if foo = bar then goto fred --> J(n,m,p)
+//
+// foo := 0                    --> Z(n)
+// foo := foo + 1              --> S(n)
+// bar := foo                  --> C(n,m)
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,6 +24,7 @@
 #include <ctype.h>
 #include <stdarg.h>
 #include <stdbool.h>
+#include <assert.h>
 
 static void fatal(const char* fmt, ...) {
   va_list ap;
@@ -16,7 +35,9 @@ static void fatal(const char* fmt, ...) {
 }
 
 static void error(unsigned lineno, const char* token, const char* fmt, ...) {
-  fprintf(stderr, "line %u: %s: ", lineno, token);
+  fprintf(stderr, "line %u: ", lineno);
+  if (token)
+    fprintf(stderr, "%s: ", token);
 
   va_list ap;
   va_start(ap, fmt);
@@ -28,13 +49,13 @@ static void error(unsigned lineno, const char* token, const char* fmt, ...) {
 }
 
 enum token_type {
-  TOK_INVALID,
+  TOK_INVALID = 256,
   TOK_EOL,
-  TOK_VAR,
+  TOK_ID,
   TOK_NUM,
-  TOK_COMMA,
   // keywords
   TOK_COPY,
+  TOK_GOTO,
   TOK_INC,
   TOK_TO,
   TOK_ZERO,
@@ -45,59 +66,98 @@ static const struct keyword {
   int token;
 } keywords[] = {
   { "copy", TOK_COPY },
+  { "goto", TOK_GOTO },
   { "inc", TOK_INC },
+  { "succ", TOK_INC },
   { "to", TOK_TO },
   { "zero", TOK_ZERO },
   { NULL, TOK_INVALID },
 };
 
-struct codegen {
-  unsigned nreg;
-  unsigned nline;
+enum inst_type { URM_NOOP, URM_ZERO, URM_SUCC, URM_COPY, URM_JUMP };
+
+struct inst {
+  unsigned type;
+  unsigned reg1;
+  unsigned reg2;
+  struct label * label;
 };
 
+#define MAX_INST (128)
+
+struct codegen {
+  unsigned nreg;
+  struct inst inst[MAX_INST];
+  unsigned ninst;
+};
+
+static void init_codegen(struct codegen * gen) {
+  gen->nreg = 0;
+  gen->inst[0].type = URM_NOOP;
+  gen->ninst = 1;
+}
+
 static void parse_statement(struct codegen *, unsigned lineno, char*);
+
+static void check_undefined_labels(struct codegen *);
+static void output_program(struct codegen *);
 
 int main(void) {
   char buf[128];
   unsigned lineno = 0;
 
   struct codegen gen;
-  gen.nline = 0;
-  gen.nreg = 0;
+  init_codegen(&gen);
 
   while (fgets(buf, sizeof buf, stdin)) {
     lineno++;
     parse_statement(&gen, lineno, buf);
   }
+
+  check_undefined_labels(&gen);
+  output_program(&gen);
+
   return 0;
 }
 
-struct token {
-  int type;
-  char* text;
+#define MAX_TEXT (32)
+
+struct lex {
+  unsigned lineno;
+  const char* next;
+  char text[MAX_TEXT];
 };
 
-static struct token get_token(char* start, char* *next);
+static void init_lex(struct lex * lex, unsigned lineno, const char* buf) {
+  lex->lineno = lineno;
+  lex->next = buf;
+  lex->text[0] = '\0';
+}
 
-static char* copy_statement(struct codegen * gen, unsigned lineno, char* args);
-static char* inc_statement(struct codegen * gen, unsigned lineno, char* args);
-static char* zero_statement(struct codegen * gen, unsigned lineno, char* args);
+static int get_token(struct lex *);
+
+static void copy_statement(struct codegen * gen, struct lex *);
+static void goto_statement(struct codegen * gen, struct lex *);
+static void inc_statement(struct codegen * gen, struct lex *);
+static void zero_statement(struct codegen * gen, struct lex *);
+static void identifier_statement(struct codegen * gen, struct lex *);
 
 static void parse_statement(struct codegen * gen, unsigned lineno, char* buf) {
-  char* start = buf;
-  char* next;
-  struct token tok = get_token(start, &next);
-  if (tok.type != TOK_EOL) {
-    switch (tok.type) {
-      case TOK_COPY: next = copy_statement(gen, lineno, next); break;
-      case TOK_INC: next = inc_statement(gen, lineno, next); break;
-      case TOK_ZERO: next = zero_statement(gen, lineno, next); break;
-      default: error(lineno, tok.text, "statement expected");
+  struct lex lex;
+  init_lex(&lex, lineno, buf);
+  int tok = get_token(&lex);
+  if (tok != TOK_EOL) {
+    switch (tok) {
+      case TOK_COPY: copy_statement(gen, &lex); break;
+      case TOK_GOTO: goto_statement(gen, &lex); break;
+      case TOK_INC: inc_statement(gen, &lex); break;
+      case TOK_ZERO: zero_statement(gen, &lex); break;
+      case TOK_ID: identifier_statement(gen, &lex); break;
+      default: error(lineno, lex.text, "statement expected");
     }
-    tok = get_token(next, &next);
-    if (tok.type != TOK_EOL)
-      error(lineno, tok.text, "end of line expected");
+    tok = get_token(&lex);
+    if (tok != TOK_EOL)
+      error(lineno, lex.text, "end of line expected");
   }
 }
 
@@ -111,7 +171,7 @@ struct symbol {
 static struct symbol symtab[MAX_SYMBOLS];
 static unsigned nsym = 0;
 
-static struct symbol * lookup(const char* name) {
+static struct symbol * lookup_symbol(const char* name) {
   for (unsigned i = 0; i < nsym; i++) {
     if (strcmp(symtab[i].name, name) == 0)
       return &symtab[i];
@@ -119,131 +179,210 @@ static struct symbol * lookup(const char* name) {
   return NULL;
 }
 
-static struct symbol * insert(const char* name, unsigned reg, unsigned lineno) {
+static struct symbol * insert_symbol(const char* name, unsigned reg, unsigned lineno) {
+  if (nsym >= MAX_SYMBOLS)
+    error(lineno, name, "too many variables");
   symtab[nsym].name = _strdup(name); // TODO: check for null
   symtab[nsym].reg = reg;
   return &symtab[nsym++];
 }
 
+struct label {
+  char* name;
+  unsigned inst;
+};
+
+#define MAX_LABELS (32)
+
+static struct label labels[MAX_LABELS];
+static unsigned nlabel = 0;
+
+static struct label * lookup_label(const char* name) {
+  for (unsigned i = 0; i < nlabel; i++) {
+    if (strcmp(labels[i].name, name) == 0)
+      return &labels[i];
+  }
+  return NULL;
+}
+
+static struct label * insert_label(const char* name, unsigned inst, unsigned lineno) {
+  if (nlabel >= MAX_LABELS)
+    error(lineno, name, "too many labels");
+  labels[nlabel].name = _strdup(name); // TODO: check for null
+  labels[nlabel].inst = inst;
+  return &labels[nlabel++];
+}
+
 static unsigned variable_register(struct codegen * gen, const char* var, unsigned lineno) {
-  struct symbol * sym = lookup(var);
+  struct symbol * sym = lookup_symbol(var);
   unsigned reg;
   if (sym)
     reg = sym->reg;
   else {
     gen->nreg++;
     reg = gen->nreg;
-    insert(var, reg, lineno);
+    insert_symbol(var, reg, lineno);
   }
   return reg;
 }
 
-static void emit(struct codegen * gen, const char* fmt, ...) {
-  gen->nline++;
-  printf("%u ", gen->nline);
-
-  va_list ap;
-  va_start(ap, fmt);
-  vprintf(fmt, ap);
-  va_end(ap);
-
-  putchar('\n');
+static struct inst * new_inst(struct codegen * gen, unsigned lineno) {
+  if (gen->ninst >= MAX_INST)
+    error(lineno, NULL, "program at maximum size");
+  return &gen->inst[gen->ninst++];
 }
 
-static char* get_variable(unsigned lineno, char* buf, char* *next);
-static void match(int expected, const char* descrip, unsigned lineno, char* buf, char* *next);
-
-static char* copy_statement(struct codegen * gen, unsigned lineno, char* args) {
-  char* var = get_variable(lineno, args, &args);
-  unsigned src = variable_register(gen, var, lineno);
-  match(TOK_TO, "TO", lineno, args, &args);
-  var = get_variable(lineno, args, &args);
-  unsigned dst = variable_register(gen, var, lineno);
-  emit(gen, "C(%u,%u)", src, dst);
-  return args;
+static void emit_zero(struct codegen * gen, unsigned reg, unsigned lineno) {
+  struct inst * i = new_inst(gen, lineno);
+  i->type = URM_ZERO;
+  i->reg1 = reg;
 }
 
-static char* zero_statement(struct codegen * gen, unsigned lineno, char* args) {
-  char* var = get_variable(lineno, args, &args);
-  unsigned reg = variable_register(gen, var, lineno);
-  emit(gen, "Z(%u)", reg);
-  return args;
+static void emit_succ(struct codegen * gen, unsigned reg, unsigned lineno) {
+  struct inst * i = new_inst(gen, lineno);
+  i->type = URM_SUCC;
+  i->reg1 = reg;
 }
 
-static char* inc_statement(struct codegen * gen, unsigned lineno, char* args) {
-  char* var = get_variable(lineno, args, &args);
-  unsigned reg = variable_register(gen, var, lineno);
-  emit(gen, "S(%u)", reg);
-  return args;
+static void emit_copy(struct codegen * gen, unsigned src, unsigned dst, unsigned lineno) {
+  struct inst * i = new_inst(gen, lineno);
+  i->type = URM_COPY;
+  i->reg1 = src;
+  i->reg2 = dst;
 }
 
-static char* get_variable(unsigned lineno, char* buf, char* *next) {
-  struct token tok = get_token(buf, next);
-  if (tok.type != TOK_VAR)
-    error(lineno, tok.text, "variable expected");
-  return tok.text;
+static void emit_jump(struct codegen * gen, unsigned reg1, unsigned reg2, struct label * label, unsigned lineno) {
+  struct inst * i = new_inst(gen, lineno);
+  i->type = URM_JUMP;
+  i->reg1 = reg1;
+  i->reg2 = reg2;
+  i->label = label;
 }
 
-static void match(int expected, const char* descrip, unsigned lineno, char* buf, char* *next) {
-  struct token tok = get_token(buf, next);
-  if (tok.type != expected)
-    error(lineno, tok.text, "%s expected", descrip);
+static void check_undefined_labels(struct codegen * gen) {
+  unsigned err = 0;
+  for (unsigned i = 1; i < gen->ninst; i++) {
+    if (gen->inst[i].type == URM_JUMP && gen->inst[i].label->inst == 0) {
+      fprintf(stderr, "undefined label: %s\n", gen->inst[i].label->name);
+      err++;
+    }
+  }
+  if (err)
+    fatal("%u undefined labels\n", err);
 }
 
-static char* demarcate_token(char* start, char* *next);
+static void output_program(struct codegen * gen) {
+  for (unsigned i = 1; i < gen->ninst; i++) {
+    printf("%u ", i);
+    struct inst * inst = &gen->inst[i];
+    switch (inst->type) {
+      case URM_ZERO: printf("Z(%u)", inst->reg1); break;
+      case URM_SUCC: printf("S(%u)", inst->reg1); break;
+      case URM_COPY: printf("C(%u,%u)", inst->reg1, inst->reg2); break;
+      case URM_JUMP: printf("J(%u,%u,%u)", inst->reg1, inst->reg2, inst->label->inst); break;
+      default: assert(0 && "unexpected URM instruction"); break;
+    }
+    putchar('\n');
+  }
+}
 
-static struct token get_token(char* start, char* *next) {
-  struct token tok;
+static void get_variable(struct lex *);
+static void match(int expected, const char* descrip, struct lex *);
 
-  tok.text = demarcate_token(start, next);
-  if (*tok.text == '\0' || *tok.text == '\n' || *tok.text == ';') {
-    tok.type = TOK_EOL;
-    return tok;
+static void zero_statement(struct codegen * gen, struct lex * lex) {
+  get_variable(lex);
+  unsigned reg = variable_register(gen, lex->text, lex->lineno);
+  emit_zero(gen, reg, lex->lineno);
+}
+
+static void inc_statement(struct codegen * gen, struct lex * lex) {
+  get_variable(lex);
+  unsigned reg = variable_register(gen, lex->text, lex->lineno);
+  emit_succ(gen, reg, lex->lineno);
+}
+
+static void copy_statement(struct codegen * gen, struct lex * lex) {
+  get_variable(lex);
+  unsigned src = variable_register(gen, lex->text, lex->lineno);
+  match(TOK_TO, "TO", lex);
+  get_variable(lex);
+  unsigned dst = variable_register(gen, lex->text, lex->lineno);
+  emit_copy(gen, src, dst, lex->lineno);
+}
+
+static void goto_statement(struct codegen * gen, struct lex * lex) {
+  match(TOK_ID, "label", lex);
+  struct label * label = lookup_label(lex->text);
+  if (label == NULL)
+    label = insert_label(lex->text, 0, lex->lineno);
+  emit_jump(gen, 1, 1, label, lex->lineno);
+}
+
+static void identifier_statement(struct codegen * gen, struct lex * lex) {
+  char id[MAX_TEXT];
+  strcpy(id, lex->text);
+  int tok = get_token(lex);
+  if (tok == ':') {
+    struct label * label = lookup_label(id);
+    if (label) {
+      if (label->inst)
+        error(lex->lineno, id, "label already defined");
+      label->inst = gen->ninst;
+    }
+    else
+      insert_label(id, gen->ninst, lex->lineno);
+  }
+  else
+    error(lex->lineno, id, "label definition expected");
+}
+
+static void match(int expected, const char* descrip, struct lex * lex) {
+  int tok = get_token(lex);
+  if (tok != expected)
+    error(lex->lineno, lex->text, "%s expected", descrip);
+}
+
+static void get_variable(struct lex * lex) {
+  match(TOK_ID, "variable", lex);
+}
+
+static int get_token(struct lex * lex) {
+  while (isspace(*lex->next))
+    lex->next++;
+
+  const char* start = lex->next;
+  int tok;
+
+  if (isalpha(*lex->next)) {
+    do {
+      lex->next++;
+    } while (isalnum(*lex->next));
+    tok = TOK_ID;
+  }
+  else if (isdigit(*lex->next)) {
+    do {
+      lex->next++;
+    } while (isdigit(*lex->next));
+    tok = TOK_NUM;
+  }
+  else if (*lex->next == '\0' || *lex->next == '\n' || *lex->next == ';')
+    tok = TOK_EOL;
+  else {
+    tok = *lex->next;
+    lex->next++;
   }
 
-  if (*tok.text == ',') {
-    tok.type = TOK_COMMA;
-    return tok;
-  }
+  strncpy(lex->text, start, lex->next - start);
+  lex->text[lex->next - start] = '\0';
 
-  for (const struct keyword * k = keywords; k->name; k++) {
-    if (strcmp(k->name, start) == 0) {
-      tok.type = k->token;
-      return tok;
+  if (tok == TOK_ID) {
+    for (const struct keyword * k = keywords; k->name; k++) {
+      if (strcmp(k->name, lex->text) == 0) {
+        tok = k->token;
+        break;
+      }
     }
   }
 
-  bool alpha = true;
-  bool numeric = true;
-  for (const char* p = start; *p; p++) {
-    if (!isalpha(*p))
-      alpha = false;
-    if (!isdigit(*p))
-      numeric = false;
-  }
-  if (alpha) {
-    tok.type = TOK_VAR;
-    return tok;
-  }
-  if (numeric) {
-    tok.type = TOK_NUM;
-    return tok;
-  }
-  tok.type = TOK_INVALID;
   return tok;
-}
-
-static char* demarcate_token(char* start, char* *next) {
-  while (isspace(*start))
-    start++;
-  char *end = start;
-  while (*end != '\0' && !isspace(*end))
-    end++;
-  if (*end == '\0')
-    *next = end;
-  else {
-    *end = '\0';
-    *next = end + 1;
-  }
-  return start;
 }
